5ce3b08c9c024ce63bee101b2f36fe386c7438ee
Rodrigo Gutierrez Cruz <134517998+ItzRodri@users.noreply.github.com>
2025-04-16 14:48:33 -0400

    part1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,20 +1,33 @@
-import Course from './Course.jsx'
+import Course from "./Course.jsx";
+import Content from "./Content.jsx";
+import Total from "./Total.jsx";
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
       <Course course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,13 +1,19 @@
-const Content = ({ parts1, parts2, parts3, exercises1, exercises2, exercises3 }) => {
+import Part from "./Part.jsx";
+const Content = ({
+  parts1,
+  parts2,
+  parts3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
   return (
     <div>
-     
-      <p> {parts1} {exercises1} </p> 
-      <p> {parts2} {exercises2} </p> 
-      <p> {parts3} {exercises3} </p> 
+      <Part part={parts1} exercises={exercises1} />
+      <Part part={parts2} exercises={exercises2} />
+      <Part part={parts3} exercises={exercises3} />
     </div>
-  )
-}
+  );
+};
 
-
-export default Content
+export default Content;
